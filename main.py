49230928341d091a4bb0bb14cacfe845c5bd9761
TeepaBlue <teepa256@protonmail.com>
2020-04-07 20:55:42 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-import datetime 
+#Fetch the schedule of hololive live stream
+
+import datetime as dt
 import os
 import sys
 
-import 
+import scraping
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+
+    fetch_today_list()
+
+
+if __name__ == '__main__':
+    main()
