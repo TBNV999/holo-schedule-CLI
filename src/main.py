2c737997bb71adaff883e3e3d5025f8d8f56773e
TeepaBlue <teepa256@protonmail.com>
2020-04-08 11:10:27 +0900

    create src

---
+++ src/main.py
@@ -0,0 +1,19 @@
+#Fetch the schedule of hololive live stream
+
+import datetime as dt
+import os
+import sys
+
+import scraping
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+
+    fetch_today_list()
+
+
+if __name__ == '__main__':
+    main()
