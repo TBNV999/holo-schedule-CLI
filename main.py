3cec94b65aee165278dc77ef798ce4ac7c826a6f
TeepaBlue <teepa256@protonmail.com>
2020-04-10 20:22:21 +0900

    update

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ from src.util import *
 def main():
 
     source_html = fetch_source_html()
-    show_list(source_html)
+    time_list, stream_members_list, stream_url_list = get_list(source_html)
 
+    print('Time(JST)   Member  Stream_URL')
+
+    #All three lists have the same length
+    lists_length = len(time_list)
+
+    for i in range(lists_length):
+        print('{}~  {}  {}'.format(time_list[i], stream_members_list[i], stream_url_list[i]))
 
 
 if __name__ == '__main__':
