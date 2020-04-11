1dae71bf33ffcd86490c545678da1d50ac68ec96
TeepaBlue <teepa256@protonmail.com>
2020-04-11 10:27:19 +0900

    update

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def main():
     source_html = fetch_source_html()
     time_list, stream_members_list, stream_url_list = get_list(source_html)
 
-    print('Time(JST)   Member  Stream_URL')
+    print('Index   Time(JST)  Member      Stream_URL')
 
     #All three lists have the same length
     lists_length = len(time_list)
 
     for i in range(lists_length):
-        print('{}~  {}  {}'.format(time_list[i], stream_members_list[i], stream_url_list[i]))
+
+        if i < 10:
+            space = ' '
+
+        else:
+            space = ''
+
+        m_space = ' ' * (7 - len(stream_members_list[i]))
+        print('{}{}      {}~     {}{}{}'.format(i, space, time_list[i], stream_members_list[i], m_space, stream_url_list[i]))
 
 
 if __name__ == '__main__':
