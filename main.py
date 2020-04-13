3e94f07b6bf448d1d1d33ddfc811e3af550a5336
TeepaBlue <teepa256@protonmail.com>
2020-04-13 17:12:53 +0900

    update main.py

--- main.py
+++ main.py
@@ -2,17 +2,32 @@
 
 import os
 import sys
-import time
 
 from src.fetch_html import *
 from src.scraping import *
 from src.util import *
 
 
-def main():
+def main(options):
+
+    if not options is None:
+
+        if "--help" in options:
+            show_help()
+            sys.exit()
+
+        if "--eng" in options:
+            eng = True
+
+    else:
+        eng = False
 
     source_html = fetch_source_html()
     time_list, stream_members_list, stream_url_list = get_list(source_html)
+
+    if eng:
+        show_in_english(time_list, stream_members_list, stream_url_list)
+        sys.exit()
 
     print('Index   Time(JST)  Member          Stream_URL')
 
@@ -30,5 +45,21 @@ def main():
         m_space = ' ' * ( (-2 * len(stream_members_list[i]) + 14))
         print('{}{}      {}~     {}{}  {}'.format(i, space, time_list[i], stream_members_list[i], m_space, stream_url_list[i]))
 
+
 if __name__ == '__main__':
-    main()
+
+    argv = sys.argv
+
+    if len(argv) > 1:
+        argv.pop(0)
+        argv = eval_argv(argv)
+
+        if argv is None:
+            print("Error: invalid options")
+            sys.exit()
+
+        else:
+            main(argv)
+
+    else:
+        main(None)
